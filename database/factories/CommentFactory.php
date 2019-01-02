faf23215724b9cb37027ed7fe98deb714d39f11b
_tam <tamjid84@yahoo.com>
2019-01-02 14:17:58 +0600

    fix comment factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Models\Comment::class, function (Faker $faker) {
     return [
         //
-        'post_id'=>rand(100,150),
+        'post_id'=>rand(1,50),
         'author_id'=>rand(1,10),
         'comment'=>$faker->paragraph,
     ];
